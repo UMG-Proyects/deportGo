c045002e7fd260ff670a9347f5f1219c6862283b
JaimeRax <jaimecaal60@gmail.com>
2024-05-07 22:13:47 -0600

    feat login: feature screen login

---
+++ screens/login.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function login() {
+  return (
+    <SafeAreaView>
+      <View
+        style={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <View
+          style={{
+            backgroundColor: "white",
+            padding: 10,
+            width: "80%",
+            borderRadius: 10,
+          }}
+        >
+          <Text style={{ fontSize: 20 }}>Login Page</Text>
+
+          <View style={{ marginTop: 30 }}>
+            <Text>Email</Text>
+            <TextInput style={style.input} />
+          </View>
+
+          <View style={{ marginTop: 30 }}>
+            <Text>Password</Text>
+            <TextInput secureTextEntry={true} style={style.input} />
+          </View>
+
+          <View style={{ marginTop: 15 }}>
+            <TouchableOpacity style={[style.input, { width: "20%" }]}>
+              <Text style={{ color: "white", textAlign: "center" }}>Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const style = StyleSheet.create({
+  input: {
+    backgroundColor: "#6699cc",
+    padding: 7,
+    borderRadius: 6,
+  },
+});
