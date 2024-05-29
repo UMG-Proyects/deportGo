c060cfbe1e6c9b14048b2199f04430d0491d70e4
JaimeRax <jaimecaal60@gmail.com>
2024-05-29 05:09:47 -0600

    :sparkles: (feat): screen Eventos finish

--- screens/ScreenEvento.jsx
+++ screens/ScreenEvento.jsx
@@ -10,6 +10,7 @@ import {
   Modal,
   TouchableOpacity,
   Platform,
+  ScrollView,
 } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import DateTimePicker from "@react-native-community/datetimepicker";
@@ -24,7 +25,8 @@ export default function Eventos() {
   const [filteredEventos, setFilteredEventos] = useState([]);
   const [search, setSearch] = useState("");
   const [modalVisible, setModalVisible] = useState(false);
-  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [showDatePickerInicio, setShowDatePickerInicio] = useState(false);
+  const [showDatePickerFinal, setShowDatePickerFinal] = useState(false);
   const [showTimePicker, setShowTimePicker] = useState(false);
   const [currentEvento, setCurrentEvento] = useState({
     id: null,
@@ -45,6 +47,45 @@ export default function Eventos() {
   useEffect(() => {
     fetchData();
   }, []);
+
+  const deactivateEvento = async (id) => {
+    try {
+      const response = await fetch(`${API_URL}desactivarEvento/${id}`, {
+        method: "PUT",
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.status) {
+        fetchEventos();
+        Alert.alert("Éxito", "Evento desactivado exitosamente");
+      } else {
+        Alert.alert("Error", "Error al desactivar evento");
+      }
+    } catch (error) {
+      console.error("Error deactivating evento:", error);
+    }
+  };
+
+  const openModal = (
+    evento = {
+      id: null,
+      id_categoria: "",
+      id_deporte: "",
+      id_municipio: "",
+      id_patrocinador: "",
+      nombre: "",
+      descripcion: "",
+      fecha_inicio: "",
+      fecha_final: "",
+      hora: "",
+      equipos_participantes: "",
+      ubicacion: "",
+      rama: "",
+    },
+  ) => {
+    setCurrentEvento(evento);
+    setModalVisible(true);
+  };
 
   const fetchData = async () => {
     await fetchEventos();
@@ -89,7 +130,6 @@ export default function Eventos() {
     try {
       const response = await fetch(`${API_URL}listarMunicipios`);
       const data = await response.json();
-      console.log("municipio", data);
       setMunicipios(data);
     } catch (error) {
       console.error("Error fetching municipios:", error);
@@ -128,8 +168,13 @@ export default function Eventos() {
     }
 
     try {
-      const response = await fetch(`${API_URL}crearEventos`, {
-        method: "POST",
+      const method = currentEvento.id ? "PUT" : "POST";
+      const url = currentEvento.id
+        ? `${API_URL}editarEvento/${currentEvento.id}`
+        : `${API_URL}crearEventos`;
+
+      const response = await fetch(url, {
+        method: method,
         headers: {
           "Content-Type": "application/json",
         },
@@ -137,45 +182,42 @@ export default function Eventos() {
       });
       const data = await response.json();
       if (data.status) {
-        fetchEventos();
-        Alert.alert("Éxito", "Evento creado exitosamente");
+        fetchPatrocinadores();
+        Alert.alert(
+          "Éxito",
+          `Evento ${currentEvento.id ? "actualizado" : "creado"} exitosamente`,
+        );
         closeModal();
       } else {
-        Alert.alert("Error", "Error al crear evento");
+        Alert.alert(
+          "Error",
+          `Error al ${currentEvento.id ? "actualizar" : "crear"} evento`,
+        );
       }
     } catch (error) {
-      console.error("Error creating evento:", error);
+      console.error(
+        `Error ${currentEvento.id ? "updating" : "creating"} evento:`,
+        error,
+      );
     }
   };
 
   const closeModal = () => {
     setModalVisible(false);
-    setCurrentEvento({
-      id: null,
-      id_categoria: "",
-      id_deporte: "",
-      id_municipio: "",
-      id_patrocinador: "",
-      nombre: "",
-      descripcion: "",
-      fecha_inicio: "",
-      fecha_final: "",
-      hora: "",
-      equipos_participantes: "",
-      ubicacion: "",
-      rama: "",
-    });
   };
 
   const handleInputChange = (field, value) => {
     setCurrentEvento({ ...currentEvento, [field]: value });
   };
 
-  const handleDateChange = (event, selectedDate) => {
-    setShowDatePicker(Platform.OS === "ios");
+  const handleDateChange = (field, event, selectedDate) => {
+    if (Platform.OS !== "ios") {
+      setShowDatePickerInicio(false);
+      setShowDatePickerFinal(false);
+    }
     if (selectedDate) {
       const formattedDate = selectedDate.toISOString().split("T")[0];
-      handleInputChange("fecha_final", formattedDate);
+      handleInputChange(field, formattedDate);
     }
   };
 
@@ -204,191 +246,208 @@ export default function Eventos() {
         renderItem={({ item }) => (
           <View style={styles.eventoItem}>
             <Text>{item.nombre}</Text>
-            <TouchableOpacity onPress={() => editEvento(item.id)}>
-              <Text style={styles.editButton}>Editar</Text>
-            </TouchableOpacity>
-            <TouchableOpacity onPress={() => deactivateEvento(item.id)}>
-              <Text style={styles.deleteButton}>Desactivar</Text>
-            </TouchableOpacity>
+            <View style={styles.buttonContainer}>
+              <View style={{ marginRight: 8 }}>
+                <Button
+                  title="Editar"
+                  onPress={() => openModal(item)}
+                  color="#2EC4B6"
+                />
+              </View>
+              <Button
+                title="Desactivar"
+                onPress={() => deactivateEvento(item.id)}
+                color="red"
+              />
+            </View>
           </View>
         )}
       />
       <Button
         title="Crear Evento"
-        onPress={() => setModalVisible(true)}
+        onPress={() => openModal()}
         color="#2EC4B6"
       />
 
       <Modal visible={modalVisible} animationType="slide">
-        <View style={styles.modalContent}>
-          <Text style={styles.modalTitle}>Crear Evento</Text>
-          <Picker
-            selectedValue={currentEvento.id_categoria}
-            onValueChange={(itemValue) =>
-              setCurrentEvento({ ...currentEvento, id_categoria: itemValue })
-            }
-            style={styles.picker}
-          >
-            <Picker.Item label="Seleccione una Categoria" value={null} />
-            {categorias &&
-              categorias.map((categoria) => (
-                <Picker.Item
-                  key={categoria.id}
-                  label={categoria.categoria}
-                  value={categoria.id}
-                />
-              ))}
-          </Picker>
-          <Picker
-            selectedValue={currentEvento.id_deporte}
-            onValueChange={(itemValue) =>
-              setCurrentEvento({ ...currentEvento, id_deporte: itemValue })
-            }
-            style={styles.picker}
-          >
-            <Picker.Item label="Seleccione un deporte" value={null} />
-            {deportes &&
-              deportes.map((deporte) => (
-                <Picker.Item
-                  key={deporte.id}
-                  label={deporte.nombre}
-                  value={deporte.id}
-                />
-              ))}
-          </Picker>
-          <Picker
-            selectedValue={currentEvento.id_patrocinador}
-            onValueChange={(itemValue) =>
-              setCurrentEvento({ ...currentEvento, id_patrocinador: itemValue })
-            }
-            style={styles.picker}
-          >
-            <Picker.Item label="Seleccione un patrocinador" value={null} />
-            {patrocinadores &&
-              patrocinadores.map((patrocinador) => (
-                <Picker.Item
-                  key={patrocinador.id}
-                  label={
-                    patrocinador.primer_nombre +
-                    " " +
-                    patrocinador.primer_apellido
-                  }
-                  value={patrocinador.id}
-                />
-              ))}
-          </Picker>
-          <Picker
-            selectedValue={currentEvento.id_municipio}
-            onValueChange={(itemValue) =>
-              setCurrentEvento({ ...currentEvento, id_municipio: itemValue })
-            }
-            style={styles.picker}
-          >
-            <Picker.Item label="Seleccione un Municipio" value={null} />
-            {municipios &&
-              municipios.map((municipio) => (
-                <Picker.Item
-                  key={municipio.id}
-                  label={municipio.Municipio}
-                  value={municipio.id}
-                />
-              ))}
-          </Picker>
-          <TextInput
-            style={styles.input}
-            placeholder="Nombre"
-            value={currentEvento.nombre}
-            onChangeText={(text) =>
-              setCurrentEvento({ ...currentEvento, nombre: text })
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Descripción"
-            value={currentEvento.descripcion}
-            onChangeText={(text) =>
-              setCurrentEvento({ ...currentEvento, descripcion: text })
-            }
-          />
-          <TouchableOpacity
-            onPress={() => setShowDatePicker(true)}
-            style={styles.input}
-          >
-            <Text>{currentEvento.fecha_inicio || "Fecha Inicio"}</Text>
-          </TouchableOpacity>
-          {showDatePicker && (
-            <DateTimePicker
-              value={new Date()}
-              mode="date"
-              display="default"
-              onChange={handleDateChange}
+        <ScrollView style={styles.scrollView}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Crear Evento</Text>
+            <Picker
+              selectedValue={currentEvento.id_categoria}
+              onValueChange={(itemValue) => {
+                console.log("Nuevo valor de categoría:", itemValue);
+                setCurrentEvento({ ...currentEvento, id_categoria: itemValue });
+              }}
+              style={styles.picker}
+            >
+              <Picker.Item label="Seleccione una Categoria" value={null} />
+              {categorias &&
+                categorias.map((categoria) => (
+                  <Picker.Item
+                    key={categoria.id}
+                    label={categoria.categoria}
+                    value={categoria.id}
+                  />
+                ))}
+            </Picker>
+            <Picker
+              selectedValue={currentEvento.id_deporte}
+              onValueChange={(itemValue) =>
+                setCurrentEvento({ ...currentEvento, id_deporte: itemValue })
+              }
+              style={styles.picker}
+            >
+              <Picker.Item label="Seleccione un deporte" value={null} />
+              {deportes &&
+                deportes.map((deporte) => (
+                  <Picker.Item
+                    key={deporte.id}
+                    label={deporte.nombre}
+                    value={deporte.id}
+                  />
+                ))}
+            </Picker>
+            <Picker
+              selectedValue={currentEvento.id_patrocinador}
+              onValueChange={(itemValue) =>
+                setCurrentEvento({
+                  ...currentEvento,
+                  id_patrocinador: itemValue,
+                })
+              }
+              style={styles.picker}
+            >
+              <Picker.Item label="Seleccione un patrocinador" value={null} />
+              {patrocinadores &&
+                patrocinadores.map((patrocinador) => (
+                  <Picker.Item
+                    key={patrocinador.id}
+                    label={
+                      patrocinador.primer_nombre +
+                      " " +
+                      patrocinador.primer_apellido
+                    }
+                    value={patrocinador.id}
+                  />
+                ))}
+            </Picker>
+            <Picker
+              selectedValue={currentEvento.id_municipio}
+              onValueChange={(itemValue) =>
+                setCurrentEvento({ ...currentEvento, id_municipio: itemValue })
+              }
+              style={styles.picker}
+            >
+              <Picker.Item label="Seleccione un Municipio" value={null} />
+              {municipios &&
+                municipios.map((municipio) => (
+                  <Picker.Item
+                    key={municipio.id}
+                    label={municipio.Municipio}
+                    value={municipio.id}
+                  />
+                ))}
+            </Picker>
+            <TextInput
+              style={styles.input}
+              placeholder="Nombre"
+              value={currentEvento.nombre}
+              onChangeText={(text) =>
+                setCurrentEvento({ ...currentEvento, nombre: text })
+              }
             />
-          )}
-          <TouchableOpacity
-            onPress={() => setShowDatePicker(true)}
-            style={styles.input}
-          >
-            <Text>{currentEvento.fecha_final || "Fecha final"}</Text>
-          </TouchableOpacity>
-          {showDatePicker && (
-            <DateTimePicker
-              value={new Date()}
-              mode="date"
-              display="default"
-              onChange={handleDateChange}
+            <TextInput
+              style={styles.input}
+              placeholder="Descripción"
+              value={currentEvento.descripcion}
+              onChangeText={(text) =>
+                setCurrentEvento({ ...currentEvento, descripcion: text })
+              }
             />
-          )}
-          <TouchableOpacity
-            onPress={() => setShowTimePicker(true)}
-            style={styles.input}
-          >
-            <Text>{currentEvento.hora || "Hora"}</Text>
-          </TouchableOpacity>
-          {showTimePicker && (
-            <DateTimePicker
-              value={new Date()}
-              mode="time"
-              display="default"
-              onChange={handleTimeChange}
+            <TouchableOpacity
+              onPress={() => setShowDatePickerInicio(true)}
+              style={styles.input}
+            >
+              <Text>{currentEvento.fecha_inicio || "Fecha Inicio"}</Text>
+            </TouchableOpacity>
+            {showDatePickerInicio && (
+              <DateTimePicker
+                value={new Date()}
+                mode="date"
+                display="default"
+                onChange={(event, date) =>
+                  handleDateChange("fecha_inicio", event, date)
+                }
+              />
+            )}
+            <TouchableOpacity
+              onPress={() => setShowDatePickerFinal(true)}
+              style={styles.input}
+            >
+              <Text>{currentEvento.fecha_final || "Fecha final"}</Text>
+            </TouchableOpacity>
+            {showDatePickerFinal && (
+              <DateTimePicker
+                value={new Date()}
+                mode="date"
+                display="default"
+                onChange={(event, date) =>
+                  handleDateChange("fecha_final", event, date)
+                }
+              />
+            )}
+            <TouchableOpacity
+              onPress={() => setShowTimePicker(true)}
+              style={styles.input}
+            >
+              <Text>{currentEvento.hora || "Hora"}</Text>
+            </TouchableOpacity>
+            {showTimePicker && (
+              <DateTimePicker
+                value={new Date()}
+                mode="time"
+                display="default"
+                onChange={handleTimeChange}
+              />
+            )}
+            <TextInput
+              style={styles.input}
+              placeholder="Equipos participantes"
+              value={currentEvento.equipos_participantes}
+              onChangeText={(text) =>
+                setCurrentEvento({
+                  ...currentEvento,
+                  equipos_participantes: text,
+                })
+              }
             />
-          )}
-
-          <TextInput
-            style={styles.input}
-            placeholder="Equipos participantes"
-            value={currentEvento.equipos_participantes}
-            onChangeText={(text) =>
-              setCurrentEvento({
-                ...currentEvento,
-                equipos_participantes: text,
-              })
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Ubicación"
-            value={currentEvento.ubicacion}
-            onChangeText={(text) =>
-              setCurrentEvento({ ...currentEvento, ubicacion: text })
-            }
-          />
-          <TextInput
-            style={styles.input}
-            placeholder="Rama"
-            value={currentEvento.rama}
-            onChangeText={(text) =>
-              setCurrentEvento({ ...currentEvento, rama: text })
-            }
-          />
-          <Button
-            title="Agregar Evento"
-            onPress={handleCreateEvento}
-            color="#2EC4B6"
-          />
-          <View style={{ marginTop: 8 }}>
-            <Button title="Cancelar" onPress={closeModal} color="red" />
+            <TextInput
+              style={styles.input}
+              placeholder="Ubicación"
+              value={currentEvento.ubicacion}
+              onChangeText={(text) =>
+                setCurrentEvento({ ...currentEvento, ubicacion: text })
+              }
+            />
+            <TextInput
+              style={styles.input}
+              placeholder="Rama"
+              value={currentEvento.rama}
+              onChangeText={(text) =>
+                setCurrentEvento({ ...currentEvento, rama: text })
+              }
+            />
+            <Button
+              title={currentEvento.id ? "Actualizar" : "Crear"}
+              onPress={handleCreateEvento}
+              color="#2EC4B6"
+            />
+            <View style={{ marginTop: 8 }}>
+              <Button title="Cancelar" onPress={closeModal} color="red" />
+            </View>
           </View>
-        </View>
+        </ScrollView>
       </Modal>
     </View>
   );
@@ -412,15 +471,19 @@ const styles = StyleSheet.create({
     borderBottomColor: "#ccc",
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row", // Alineación horizontal de botones
   },
   editButton: {
     color: "blue",
+    marginRight: 10,
   },
   deleteButton: {
     color: "red",
   },
   modalContent: {
-    flex: 1,
     padding: 20,
   },
   modalTitle: {
@@ -449,5 +512,8 @@ const styles = StyleSheet.create({
     borderColor: "gray",
     marginBottom: 5,
     paddingHorizontal: 5,
+  },
+  scrollView: {
+    flex: 1,
   },
 });
